58f9e4b003a2d636cf5c6c787237e9fa34dd2eac
berkan akin <berkanakin45@hotmail.com>
2021-05-16 14:04:09 +0300

    first commit

---
+++ src/main/java/com/mtm/api/Country.java
@@ -0,0 +1,54 @@
+package com.mtm.api;
+
+public class Country {
+		private String country;
+		private String name;
+		private String lat;
+		private String lng;
+		
+		public Country() {
+			this.country =  "null";
+			this.name    =  "null";
+			this.lat     =  "null";
+			this.lng     =  "null";
+		}
+		public Country(String country, String name, String lat, String lng) {
+			this.country = country;
+			this.name = name;
+			this.lat = lat;
+			this.lng = lng;
+		}
+		
+		public String getCountry() {
+			return country;
+		}
+
+		public void setCountry(String country) {
+			this.country = country;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getlat() {
+			return lat;
+		}
+
+		public void setlat(String lat) {
+			this.lat = lat;
+		}
+
+		public String getIng() {
+			return lng;
+		}
+
+		public void setIng(String lng) {
+			this.lng = lng;
+		}
+		
+}
---
+++ src/main/java/com/mtm/api/Main.java
@@ -0,0 +1,26 @@
+package com.mtm.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
+
+
+
+@SpringBootApplication(exclude=SecurityAutoConfiguration.class)
+public class Main extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CountryAPI.class);
+	}
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+		
+	}
+
+}

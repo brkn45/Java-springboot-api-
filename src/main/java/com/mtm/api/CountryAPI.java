b4c0c803233ff8af475c7d4a3ba10493438819a5
berkan akin <berkanakin45@hotmail.com>
2021-05-29 10:55:10 +0300

    version 2.5

--- src/main/java/com/mtm/api/CountryAPI.java
+++ src/main/java/com/mtm/api/CountryAPI.java
@@ -3,6 +3,9 @@ package com.mtm.api;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.regex.Pattern;
 import org.json.simple.parser.ParseException;
 import org.springframework.http.HttpStatus;
@@ -38,8 +41,8 @@ public class CountryAPI {
         int i = 0, countrySize = data.getAllCountry().size();
         int count=0;
         data.getResponseCountry().clear();
-        for (i = 0; i < countrySize && count < 5; i++) {
-            if (findWord(word,data.getAllCountry().get(i).getName()) ) {
+        for (i = 0; i < countrySize; i++) {
+            if (word.length()>= data.getAllCountry().get(i).getName().length() && findWord(word,data.getAllCountry().get(i).getName()) ) {
                 data.getResponseCountry().add(data.getAllCountry().get(i));
                 flag = 1;
                 count++;
@@ -47,15 +50,33 @@ public class CountryAPI {
             
 
         }
-        if (flag == 0) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        } else {
+        System.out.println("count: "+count);
+        if(count <5) {
+        	for (i = 0; i < countrySize && data.getResponseCountry().size() <5 ; i++) {
+                if (findWord(word,data.getAllCountry().get(i).getName()) ) {
+                    data.getResponseCountry().add(data.getAllCountry().get(i));
+                    Set<Country> set = new HashSet<>(data.getResponseCountry());
+                	data.getResponseCountry().clear();
+                	data.getResponseCountry().addAll((Collection<? extends Country>) set);
+                    
+                }
+        	} 
+        	System.out.println("countson: "+count);
+        	
+        	this.setTurkey();
             return new ResponseEntity<>(data.getResponseCountry(), HttpStatus.OK);
+        }
+        else if(flag == 0) {
+        	 return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        	}
+        else {
+        	this.setTurkey();
+        	return new ResponseEntity<>(data.getResponseCountry(), HttpStatus.OK);
         }
     }
     private boolean findWord(String word,String city) {
     	for(int i=0;i<word.length() ;) {
-    		if( i< city.length() && Character.toLowerCase(word.charAt(i))==Character.toLowerCase(city.charAt(i))) {
+    		if( i<city.length() && Character.toLowerCase(word.charAt(i))==Character.toLowerCase(city.charAt(i))) {
     			i++;
     		}
     		else {
@@ -63,6 +84,17 @@ public class CountryAPI {
     		}
     	}
     	return true;
+    }
+    private void setTurkey() {
+    	int count=0;
+    	for(int i=0;i<data.responseCountry.size();i++) {
+    		if(data.responseCountry.get(i).getCountry().equals("TR")) {
+    			Country tmp = data.getResponseCountry().get(i);
+    			data.getResponseCountry().remove(i);
+    			data.getResponseCountry().add(0,tmp);
+    			
+    		}
+    	}
     }
 
 }
